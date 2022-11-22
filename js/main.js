651f356ec346473b57e2ec5e0817c6148b944837
stacy_r <stacyrileyca@gmail.com>
2022-11-22 16:01:44 -0500

    Added images to home page and updated navbar in js

---
+++ js/main.js
@@ -0,0 +1,12 @@
+const x = document.getElementById("myLinks");
+
+//Navbar hamburger open/close movement
+  const navBar = function(){
+    if(x.style.display === "block"){
+      x.style.display = "none";
+    } else {
+      x.style.display = "block";
+    }
+  }
+
+ 
